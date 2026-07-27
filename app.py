865f2d5a23976d75394516a39202efda185e15f3
BINAYAK <binayakchoudhury08@gmail.com>
2026-07-27 23:36:22 +0530

    update project

--- app.py
+++ app.py
@@ -748,7 +748,7 @@ def export_party_transport_excel():
     ws.append([
         "Date", "SL No", "Party Name", "Challan No", "Vehicle No",
         "Slip No", "HSD Qty", "Rate", "HSD Amount",
-        "Cash Taken", "Final Amount"
+        "Diesel", "Final Amount"
     ])
 
     total_hsd = 0
@@ -1010,7 +1010,7 @@ def export_party_transport_pdf():
     data = [[
         "Date", "SL", "Challan", "Vehicle",
         "Slip", "HSD Qty (L)", "Rate", "HSD Amt",
-        "Cash Taken", "Final Amt"
+        "Diesel", "Final Amt"
     ]]
 
     total_hsd = 0
